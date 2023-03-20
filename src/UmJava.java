549dea234dd02a67e81b7432384754aec3315fe0
Guilherme Milani <gui26milani@gmail.com>
2023-03-19 22:18:41 -0300

    Feat:Correção da questão um

--- src/UmJava.java
+++ src/UmJava.java
@@ -4,7 +4,7 @@ public class UmJava {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] num = new int[5];
-        int somaNum = 0;
+        double somaNum = 0;
         for (int i = 0; i < 5; i++) {
             System.out.println("Escreva o " + (i+1) + "° número:");
             num[i] = sc.nextInt();
